2f7ddd7005bc8ecac6060c6382ec7c2bec780890
Danish Khan <khand000786@gmail.com>
2025-11-22 11:56:11 +0000

    I thinkit ne some key?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -5,7 +5,7 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = {"apiKey":"FAKE_API_KEY","authDomain":"notesplus-pro.firebaseapp.com","projectId":"notesplus-pro","storageBucket":"notesplus-pro.appspot.com","messagingSenderId":"FAKE_SENDER_ID","appId":"FAKE_APP_ID","measurementId":"FAKE_MEASUREMENT_ID"};
+const firebaseConfig = {"apiKey":"FIREBASE_API_KEY","authDomain":"FIREBASE_AUTH_DOMAIN","projectId":"FIREBASE_PROJECT_ID","storageBucket":"FIREBASE_STORAGE_BUCKET","messagingSenderId":"FIREBASE_MESSAGING_SENDER_ID","appId":"FIREBASE_APP_ID","measurementId":"FIREBASE_MEASUREMENT_ID"};
 
 let app: ReturnType<typeof initializeApp>;
 let auth: ReturnType<typeof getAuth>;
