d45664d201adb8fcc5dce1d24cd89c7e97e6e811
Danish Khan <khand000786@gmail.com>
2025-11-22 10:41:16 +0000

    keep the background of the header to same as iphone new background liqui

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -54,7 +54,7 @@ export default function Header() {
   };
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/60 backdrop-blur-lg supports-[backdrop-filter]:bg-background/60">
       <div className="container mx-auto flex h-16 items-center">
         <div className="mr-auto md:mr-4">
           <Logo />
